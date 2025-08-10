d2c1360612cd57aa0c6afc59f3f31a25d666189f
Ayman428 <asayed@tanfeethi.com.sa>
2025-08-10 15:44:00 +0300

    make Static Sections
    start make Static Section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,14 @@
 import Container from "../components/common/Container"
 import FAQs from "../components/common/FAQs";
+import Hero from "../components/once/Hero";
 import Sponsors from "../components/once/Sponsors"
+import StaticSection from "../components/once/StaticSection";
 
 const Home = () => {
     return (
         <div>
+            <Hero />
+            <StaticSection />
             <FAQs />
             <Sponsors />
             <Container />
