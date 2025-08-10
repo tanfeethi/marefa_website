ae6d73e3d0fa2d70e72c84b7932c420a3069f9ef
Ayman428 <asayed@tanfeethi.com.sa>
2025-08-10 16:07:18 +0300

    make Services Section

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,8 +1,10 @@
 import Container from "../components/common/Container"
+import AllServices from "../components/once/AllServices"
 
 const About = () => {
     return (
         <div>
+            <AllServices />
             <Container />
         </div>
     )
